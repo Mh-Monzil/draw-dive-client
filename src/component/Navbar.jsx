6bca889c884aa33ecb497d76c0d11f486f72e7fe
Mh-Monzil <monzil246@gmail.com>
2024-04-26 18:21:24 +0600

    first commit

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+import logo from '../assets/dd-logo.png';
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <Link to='/'>Home</Link>
+            <Link to='/'>Login</Link>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">
+            <img src={logo} className="w-12 h-12" />
+            <p className="text-3xl font-bold">DrawDrive</p>
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+            <Link to='/'>Home</Link>
+            <Link to='/'>Login</Link>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Login</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

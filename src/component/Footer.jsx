064b78cf6978bc4a69ec1b216c50aacd18e13de5
Mh-Monzil <monzil246@gmail.com>
2024-04-30 19:29:42 +0600

    footer updated

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -17,19 +17,14 @@ const Footer = () => {
         </aside>
         <nav>
           <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Comfortable</a>
-          <a className="link link-hover">Extra Security</a>
-          <a className="link link-hover">Best Price</a>
-          <a className="link link-hover">Strategic Location</a>
+          <a className="link link-hover">Art Supplies Finder</a>
+          <a className="link link-hover">Artist Showcase</a>
         </nav>
         <nav>
           <h6 className="footer-title">Company</h6>
           <a className="link link-hover">About us</a>
-          <Link to="/contact-us">
+          <Link>
             <a className="link link-hover">Contact</a>
-          </Link>
-          <Link to="/user-profile">
-            <a className="link link-hover">Profile</a>
           </Link>
         </nav>
         <nav>
